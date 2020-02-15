b8ba5845091537458b704d274cd57712f348df7d
Graham Lee <leeg@labrary.online>
2020-02-15 14:11:57 +0000

    Add react-images-upload component to, well, to upload images.
    Co-Authored-By: Lewis Belcher <belcher_lewis@protonmail.com>

--- frontengels/src/App.js
+++ frontengels/src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
+import ImageUploader from 'react-images-upload';
 import logo from './img/logo.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        picture: null,
+      };
+    }
+    
+    render() {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+          <ImageUploader withIcon
+            buttonText='Choose Image'
+            onChange={(picture) => { this.setState({ picture }); }}
+            imgExtension={['.jpg', '.png']}
+          />
+        </div>
+      );
+    }
 }
 
 export default App;
