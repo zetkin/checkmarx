655bef5d9e6ab1cc5662546cf148ab03e767c636
Lewis Belcher <belcher_lewis@protonmail.com>
2021-04-10 13:30:39 +0200

    Clean up App.js

--- frontengels/src/App.js
+++ frontengels/src/App.js
@@ -1,104 +1,142 @@
 import React from 'react';
-import ImageUploader from 'react-images-upload';
 import axios from 'axios';
 import logo from './img/logo.png';
 import Webcam from "react-webcam";
 import './App.css';
 
 function b64toBlob(byteCharacters, contentType, sliceSize) {
-        contentType = contentType || '';
-        sliceSize = sliceSize || 512;
+    contentType = contentType || '';
+    sliceSize = sliceSize || 512;
 
-        byteCharacters = byteCharacters.slice("data:image/jpeg;base64,".length)
-        var byteCharacters = atob(byteCharacters);
-        var byteArrays = [];
+    var byteCharacters2 = byteCharacters.slice("data:image/jpeg;base64,".length)
+    var byteCharacters3 = atob(byteCharacters2);
+    var byteArrays = [];
 
-        for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-            var slice = byteCharacters.slice(offset, offset + sliceSize);
+    for (var offset = 0; offset < byteCharacters3.length; offset += sliceSize) {
+        var slice = byteCharacters3.slice(offset, offset + sliceSize);
 
-            var byteNumbers = new Array(slice.length);
-            for (var i = 0; i < slice.length; i++) {
-                byteNumbers[i] = slice.charCodeAt(i);
-            }
-
-            var byteArray = new Uint8Array(byteNumbers);
-
-            byteArrays.push(byteArray);
+        var byteNumbers = new Array(slice.length);
+        for (var i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i);
         }
 
-      var blob = new Blob(byteArrays, {type: contentType});
-      return blob;
+        var byteArray = new Uint8Array(byteNumbers);
+
+        byteArrays.push(byteArray);
+    }
+
+    var blob = new Blob(byteArrays, {
+        type: contentType
+    });
+    return blob;
 }
 
 class App extends React.Component {
 
-    constructor(props) {
-      super(props);
-      this.state = {
-        picture: null,
-        result: null,
-      };
-      this.webcamRef = React.createRef();
-    }
-
-    render() {
-      const videoConstraints = {
-        width: 1280,
-        height: 720,
-        facingMode: "environment"
-      };
-      const capture = () => {
-        const imageSrc = this.webcamRef.current.getScreenshot();
-        this.setState({picture: imageSrc});
-      };
-      const paragraphs = [];
-      if (this.state.result) {
-        if (this.state.result.length) {
-          for (var i = 0; i < this.state.result.length; i++) {
-            paragraphs.push(<p key={this.state.result[i]}>{this.state.result[i]} was Marxed</p>);
-          }
-        } else {
-          paragraphs.push(<p>No result found</p>)
+        constructor(props) {
+            super(props);
+            this.state = {
+                picture: null,
+                result: null,
+            };
+            this.webcamRef = React.createRef();
         }
-      }
-      const image = this.state.picture ?
-        <img src={this.state.picture} alt="webcam capture" /> : <Webcam
-        audio={false}
-        ref={this.webcamRef}
-        screenshotFormat="image/jpeg"
-        width="100%"
-        videoConstraints={videoConstraints}
-        justifyContent="left"
-      />
-      const scan = () => {
-                              var form = new FormData();
-                              form.append("image", b64toBlob(this.state.picture));
-                              axios({
-                                                          method: "post",
-                                                          url: "/scan",
-                                                          data: form,
-                                                          headers: { 'Content-Type': 'multipart/form-data' }
-                                                      })
-                              .then((res) => {
-                                                          this.setState({ picture: null, result: res.data.result });
-                                                      })
-                              .catch((err) => {
-                                                          console.error(err);
-                                                      });
-                          }
 
-      return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-          </header>
-          {image}
-          <button onClick={capture}>Capture photo</button>
-          <button onClick={scan} disabled={!this.state.picture}>Check Marx</button>
-          {paragraphs}
-        </div>
-      );
-    }
-}
+        render() {
+                const videoConstraints = {
+                    width: 1280,
+                    height: 720,
+                    facingMode: "environment"
+                };
+                const capture = () => {
+                    const imageSrc = this.webcamRef.current.getScreenshot();
+                    this.setState({
+                        picture: imageSrc
+                    });
+                };
+                const paragraphs = [];
+                if (this.state.result) {
+                    if (this.state.result.length) {
+                        for (var i = 0; i < this.state.result.length; i++) {
+                            paragraphs.push( < p key = {
+                                    this.state.result[i]
+                                } > {
+                                    this.state.result[i]
+                                }
+                                was Marxed < /p>);
+                            }
+                        } else {
+                            paragraphs.push( < p > No result found < /p>)
+                            }
+                        }
+                        const image = this.state.picture ?
+                            <
+                            img src = {
+                                this.state.picture
+                            }
+                        alt = "webcam capture" / >: < Webcam
+                        audio = {
+                            false
+                        }
+                        ref = {
+                            this.webcamRef
+                        }
+                        screenshotFormat = "image/jpeg"
+                        width = "100%"
+                        videoConstraints = {
+                            videoConstraints
+                        }
+                        justifyContent = "left" /
+                            >
+                            const scan = () => {
+                                var form = new FormData();
+                                form.append("image", b64toBlob(this.state.picture));
+                                axios({
+                                        method: "post",
+                                        url: "/scan",
+                                        data: form,
+                                        headers: {
+                                            'Content-Type': 'multipart/form-data'
+                                        }
+                                    })
+                                    .then((res) => {
+                                        this.setState({
+                                            picture: null,
+                                            result: res.data.result
+                                        });
+                                    })
+                                    .catch((err) => {
+                                        console.error(err);
+                                    });
+                            }
 
-export default App;
+                        return ( <
+                            div className = "App" >
+                            <
+                            header className = "App-header" >
+                            <
+                            img src = {
+                                logo
+                            }
+                            className = "App-logo"
+                            alt = "logo" / >
+                            <
+                            /header> {
+                                image
+                            } <
+                            button onClick = {
+                                capture
+                            } > Capture photo < /button> <
+                            button onClick = {
+                                scan
+                            }
+                            disabled = {!this.state.picture
+                            } > Check Marx < /button> {
+                                paragraphs
+                            } <
+                            /div>
+                        );
+                    }
+                }
+
+                export default App;
